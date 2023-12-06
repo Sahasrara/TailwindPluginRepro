05039001acd41a758c4d6d7c6d282397aa8c7c1d
Eric Fulton <eric.fulton@unity3d.com>
2023-12-06 10:36:15 -0800

    Adding vscode settings

--- src/lib/components/Topbar.svelte
+++ src/lib/components/Topbar.svelte
@@ -1,4 +1,4 @@
 <script lang="ts">
 </script>
 
-<div class="text-slate-400 bg-cyan-100 top-1 dark:bg-cyan-800">buttsd</div>
+<div class="text-slate-400 bg-cyan-100 top-1 dark:bg-cyan-800">sorting doesn't work</div>
